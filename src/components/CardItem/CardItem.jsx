2a3ce33962967732e71ed80c1412d02028cd809c
Tomás Aronna San Martín <tomasaronna@gmail.com>
2021-08-29 19:39:58 -0300

    Agregué la plataforma

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -11,6 +11,7 @@ const CardItem = ({ data }) => {
         <Card.Content>
           <Image src={data.img} alt="Wallpaper del juego" />
           <Card.Header id="title">{data.name}</Card.Header>
+          <Card.Meta id="info">{data.platform}</Card.Meta>
           <Card.Meta id="info">{data.price}</Card.Meta>
           <Card.Description id="descripcion">
             {data.description}
