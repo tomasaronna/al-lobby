44210948b66c7417a757d3eb0259b0f052520657
Tomás Aronna San Martín <tomasaronna@gmail.com>
2021-08-31 22:17:02 -0300

    Apliqué el React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
-import ItemListContainer from "./components/itemListContainer/itemListContainer";
+import ItemListContainer from "./components/itemListContainer/ItemListContainer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Inicio from "./components/Views/Inicio";
+import AcercaDe from "./components/Views/Acerca-de";
+import Contacto from "./components/Views/Contacto";
+import ItemDetail from "./components/Views/itemDetail/itemDetail";
 
 const App = () => {
   return (
-    <div className="body">
-      <NavBar />
-      <section className="recomendados">
-        <ItemListContainer />
-      </section>
-    </div>
+    <Router>
+      <div className="body">
+        <NavBar />
+        <Switch>
+          <Route path="/inicio" exact component={Inicio} />
+          <Route path="/acerca-de" component={AcercaDe} />
+          <Route path="/contacto" component={Contacto} />
+          <Route path="/detail/:id" component={ItemDetail} />
+          <section className="recomendados">
+            <ItemListContainer />
+          </section>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
