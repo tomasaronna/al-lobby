30432e44ca1cabf152b7fa7d75870bf44b98e141
Tomás Aronna San Martín <tomasaronna@gmail.com>
2021-08-31 22:15:23 -0300

    Cree la view

---
+++ src/components/Views/Contacto.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Contacto = () => {
+  return (
+    <div>
+      <h1>Contacto</h1>
+    </div>
+  );
+};
+export default Contacto;
---
+++ src/components/Views/Inicio.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ItemListContainer from "../itemListContainer/ItemListContainer";
+
+const Inicio = () => {
+  return (
+    <section className="recomendados">
+      <ItemListContainer />
+    </section>
+  );
+};
+export default Inicio;
