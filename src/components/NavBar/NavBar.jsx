a86687789c5cab9a34b61a0fed95f0aef531c6f9
Tomás Aronna San Martín <tomasaronna@gmail.com>
2021-08-31 22:16:13 -0300

    Modifiqué los Nav.Link

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,30 +1,36 @@
 import React from "react";
-// import Nav from "react-bootstrap/Nav";
 import { Nav, NavDropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Gnome from "../../Assets/Gnome.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import "./NavBar.css";
+import { NavLink } from "react-router-dom";
 
-const Navigation = () => {
+const NavBar = () => {
   return (
     <div>
       <Nav className="justify-content-center nav" activeKey="/home">
         <Nav.Item>
-          <Nav.Link href="/home">Inicio</Nav.Link>
+          <NavLink to="/inicio" id="link">
+            Inicio
+          </NavLink>
         </Nav.Item>
-        <NavDropdown title="Productos" id="collasible-nav-dropdown">
+        <NavDropdown title="Productos" id="link">
           <NavDropdown.Item href="#action/3.1">PC</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">PlayStation 5</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.3">Xbox Series X</NavDropdown.Item>
         </NavDropdown>
         <img src={Gnome} className="logo" alt="logo" />
         <Nav.Item>
-          <Nav.Link>Acerca de</Nav.Link>
+          <NavLink to="/acerca-de" id="link">
+            Acerca de
+          </NavLink>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link>Contacto</Nav.Link>
+          <NavLink to="/contacto" id="link">
+            Contacto
+          </NavLink>
         </Nav.Item>
         <FontAwesomeIcon icon={faShoppingCart} className="cartIcon" />
       </Nav>
@@ -32,4 +38,4 @@ const Navigation = () => {
   );
 };
 
-export default Navigation;
+export default NavBar;
