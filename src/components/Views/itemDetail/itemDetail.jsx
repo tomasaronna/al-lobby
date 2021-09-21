5d71f805f6a38226c1f9ea20c8ff202e577103a9
Tomás Aronna San Martín <tomasaronna@gmail.com>
2021-09-21 12:31:40 -0300

    Desarrollé el detalle de los juegos

--- src/components/Views/itemDetail/itemDetail.jsx
+++ src/components/Views/itemDetail/itemDetail.jsx
@@ -1,23 +1,48 @@
-import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { query, collection, getDocs } from "firebase/firestore";
+import { useParams } from "react-router";
+import db from "../../Firebase/Firebase";
 import ItemCount from "../../itemCount/itemCount";
 import "./itemDetail.css";
 
-const ItemDetail = ({ match }) => {
-  const [juego, setJuego] = useState({});
-  const juegoID = match.params.gameID;
+const ItemDetail = () => {
+  const [selectedGame, setSelectedGame] = useState([]);
+  const { id } = useParams();
+  const data = [];
+  const getDetail = async () => {
+    const datos = query(collection(db, "games"));
+
+    const querySnapshot = await getDocs(datos);
+    querySnapshot.forEach((datos) => {
+      data.push({ ...datos.data(), id: datos.id });
+    });
+    data.forEach((item) => {
+      if (item.id === id) {
+        setSelectedGame(item);
+      }
+    });
+  };
   useEffect(() => {
-    axios(
-      `https://mocki.io/v1/604b55c7-678e-4c04-8e3e-abe9ccbfee31/${juegoID}`
-    ).then((res) => setJuego(res.data));
-  }, [juegoID]);
+    getDetail();
+  }, [id]);
 
   return (
-    <div className="detalle">
-      <h1>{juego.name}</h1>
-      <img src={juego.img} alt="Wallpaper del juego" />
+    <>
+      <div className="main">
+        <img src={selectedGame.img} alt="wallpaper del juego" />
+        <h1>{selectedGame.title}</h1>
+        <h2>{selectedGame.price}</h2>
+        <h3>Plataforma: {selectedGame.platform}</h3>
+        <p>Pueden aplicarse cargos extra dependiendo el país</p>
+      </div>
+      <div className="additionalData">
+        <p>{selectedGame.description}</p>
+        <p>Modos de juego: {selectedGame.gameModes}</p>
+        <p>{selectedGame.gameInfo}</p>
+      </div>
+      <h3>Stock: {selectedGame.stock}</h3>
       <ItemCount />
-    </div>
+    </>
   );
 };
 
