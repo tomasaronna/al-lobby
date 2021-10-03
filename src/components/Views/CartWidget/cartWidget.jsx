0727e562ee4bb4a1b8129c818e7d0e1f3682fa37
Tomás Aronna San Martín <tomasaronna@gmail.com>
2021-10-03 11:02:44 -0300

    Rearmé el componente

--- src/components/Views/CartWidget/cartWidget.jsx
+++ src/components/Views/CartWidget/cartWidget.jsx
@@ -1,18 +1,22 @@
 import React, { useContext } from "react";
 import { cartContext } from "../../cartContext/cartContext";
-import "./cartWidget.css";
+// import "./cartWidget.css";
 
 const CartWidget = () => {
-  const { removeGame } = useContext(cartContext);
+  const { cart, cartQty } = useContext(cartContext);
+  console.log("CART", cart);
   return (
     <>
-      <tr>
-        <th>Título</th>
-        <th>Plataforma</th>
-        <th>Cantidad</th>
-        <th>Precio</th>
-      </tr>
-      <button onClick={removeGame()}>Quitar del carrito</button>
+      {cart.map((carrito) => {
+        return (
+          <>
+            <h1>{carrito.title}</h1>
+            <h3>{carrito.platform}</h3>
+            <h3>{cartQty}</h3>
+            <h2>{carrito.price}</h2>
+          </>
+        );
+      })}
     </>
   );
 };
