b3a4db4b474b104c53fa712b567128e569e44658
Tomás Aronna San Martín <tomasaronna@gmail.com>
2021-10-05 20:32:14 -0300

    Cree el componente

---
+++ src/components/Views/BuyConfirmation/buyConfirmation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const BuyConfirmation = () => {
+  return (
+    <div>
+      <h1>¡Gracias por comprar en Al Lobby!</h1>
+      <p>
+        La clave de activación de su producto será enviada por correo
+        electrónico
+      </p>
+    </div>
+  );
+};
+
+export default BuyConfirmation;
