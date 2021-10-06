6acebc3907a4e28350d96b9da705c3a6b037ee00
Tomás Aronna San Martín <tomasaronna@gmail.com>
2021-10-06 19:52:57 -0300

    Desarrollé la view

--- src/components/Views/Acerca-de.jsx
+++ src/components/Views/Acerca-de.jsx
@@ -4,6 +4,11 @@ const AcercaDe = () => {
   return (
     <div>
       <h1>ACERCA DE</h1>
+      <p>
+        Cansados del monopolio de los grandes monstruos de la industria, un
+        grupo de amigos se juntaron para formar "Al Lobby", una tienda virtual
+        de videojuegos para amantes del gaming.
+      </p>
     </div>
   );
 };
--- src/components/Views/Contacto.jsx
+++ src/components/Views/Contacto.jsx
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Views.css";
 
 const Contacto = () => {
+  const [inputName, setInputName] = useState("");
+  const [inputLastName, setInputLastName] = useState("");
+  const [inputMail, setInputMail] = useState("");
+
+  const handleOnChange = (e) => {
+    if (e.target.name === "nombre") {
+      setInputName(e.target.value);
+    } else if (e.target.name === "apellido") {
+      setInputLastName(e.target.value);
+    } else if (e.target.name === "mail") {
+      setInputMail(e.target.value);
+    }
+  };
+
+  const prevDefault = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-      <h1>Contacto</h1>
+      <h1>¡Contactanos!</h1>
+      <p>Completá los siguientes campos para enviarnos tu consulta.</p>
+      <form>
+        <div className="name">
+          <label htmlFor="nombre">Nombre:</label>
+          <input
+            type="text"
+            name="nombre"
+            id="nombre"
+            placeholder="Ej: Tomás"
+            value={inputName}
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="lastName">
+          <label htmlFor="apellido">Apellido</label>
+          <input
+            type="text"
+            name="apellido"
+            id="apellido"
+            placeholder="Ej: Aronna San Martín"
+            value={inputLastName}
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="mail">
+          <label htmlFor="mail">Mail</label>
+          <input
+            type="email"
+            name="mail"
+            id="mail"
+            placeholder="taronna@gmail.com"
+            value={inputMail}
+            onChange={handleOnChange}
+          />
+        </div>
+        <div>
+          <textarea
+            className="consulta"
+            placeholder="escriba su consulta aquí"
+          ></textarea>
+        </div>
+        <button type="submit" className="send" onClick={prevDefault}>
+          Enviar
+        </button>
+      </form>
     </div>
   );
 };
